c5d125e3f0e67c485c950290f0b5b01533f8d17e
Roberto <robertogianocca@outlook.com>
2025-12-10 14:03:33 +0100

    Impostato un database di video semplice.

---
+++ src/data/video-data-base.js
@@ -0,0 +1,29 @@
+export const videoDataBase = [
+  {
+    id: "sugar-mama",
+    vimeoId: "1132948199",
+    title: "Sugar Mama",
+    subtitle: "A music video for Matt Pascale & The Stomps",
+    // description: videoDescriptions["sugar-mama"],
+    // cover: "/video/sugar-mama/sugar-mama-cover.jpg",
+    // thumbnail: "/video-thumbnails/sugar-mama-thumb_01.jpg",
+  },
+  {
+    id: "met-parachute",
+    vimeoId: "676799834",
+    title: "Met Parachute MCR",
+    subtitle: "",
+    // description: videoDescriptions["sugar-mama"],
+    // cover: "/video/sugar-mama/sugar-mama-cover.jpg",
+    // thumbnail: "/video-thumbnails/sugar-mama-thumb_01.jpg",
+  },
+  {
+    id: "carie",
+    vimeoId: "676793805",
+    title: "CARIE",
+    subtitle: "",
+    // description: videoDescriptions["sugar-mama"],
+    // cover: "/video/sugar-mama/sugar-mama-cover.jpg",
+    // thumbnail: "/video-thumbnails/sugar-mama-thumb_01.jpg",
+  },
+];

944e5124cc6d0757c488da009b44aaf16d4e1849
Roberto <robertogianocca@outlook.com>
2025-12-13 19:41:11 +0100

    Base layout for the new player.

--- src/app/player-test/Player.jsx
+++ src/app/player-test/Player.jsx
@@ -50,60 +50,66 @@ export default function Player({ vimeoId }) {
       {/*                           PLAYER                             */}
       {/* ============================================================ */}
       <div>
-        <MediaPlayer
-          key={vimeoId}
-          aspectRatio="16/9"
-          ref={player}
-          src={src}
-          // playsInline
-        >
+        <MediaPlayer key={vimeoId} aspectRatio="16/9" ref={player} src={src} playsInline>
           <MediaProvider />
           <Controls.Root className="vds-controls">
-            <Controls.Group className="vds-controls-group">Top Controls Group</Controls.Group>
-            <div className="vds-controls-spacer" />
-            <Controls.Group className="vds-controls-group">Center Controls Group</Controls.Group>
-            <div className="vds-controls-spacer" />
             <Controls.Group className="vds-controls-group">
-              <TimeSlider.Root className="vds-time-slider vds-slider">
-                <TimeSlider.Track className="vds-slider-track" />
-                <TimeSlider.TrackFill className="vds-slider-track-fill vds-slider-track" />
-                <TimeSlider.Progress className="vds-slider-progress vds-slider-track" />
-                <TimeSlider.Thumb className="vds-slider-thumb" />
-              </TimeSlider.Root>
-              <PlayButton className="vds-button">
-                {isPaused ? (
-                  <PlayIcon className="play-icon vds-icon" />
-                ) : (
-                  <PauseIcon className="pause-icon vds-icon" />
-                )}
-              </PlayButton>
-
-              <MuteButton className="vds-button">
-                {isMuted || volume == 0 ? (
-                  <MuteIcon className="mute-icon vds-icon" />
-                ) : volume < 0.5 ? (
-                  <VolumeLowIcon className="volume-low-icon vds-icon" />
-                ) : (
-                  <VolumeHighIcon className="volume-high-icon vds-icon" />
-                )}
-              </MuteButton>
-              <VolumeSlider.Root className="vds-slider">
-                <VolumeSlider.Track className="vds-slider-track" />
-                <VolumeSlider.TrackFill className="vds-slider-track-fill vds-slider-track" />
-                <VolumeSlider.Thumb className="vds-slider-thumb" />
-              </VolumeSlider.Root>
-              <div className="vds-time-group">
-                <Time className="vds-time" />
-                <div className="vds-time-divider">/</div>
-                <Time className="vds-time" />
+              <div className="buttons-bar">
+                {/* ========== Left buttons containers ========== */}
+                <div className="flex flex-row items-center">
+                  <PlayButton className="vds-button">
+                    {isPaused ? (
+                      <PlayIcon className="play-icon vds-icon" />
+                    ) : (
+                      <PauseIcon className="pause-icon vds-icon" />
+                    )}
+                  </PlayButton>
+                  <MuteButton className="vds-button">
+                    {isMuted || volume == 0 ? (
+                      <MuteIcon className="mute-icon vds-icon" />
+                    ) : volume < 0.5 ? (
+                      <VolumeLowIcon className="volume-low-icon vds-icon" />
+                    ) : (
+                      <VolumeHighIcon className="volume-high-icon vds-icon" />
+                    )}
+                  </MuteButton>
+                  {/* ========== VOLUME SLIDER CONTAINER ========== */}
+                  <div className="w-25 hidden md:flex">
+                    <VolumeSlider.Root className="vds-slider">
+                      <VolumeSlider.Track className="vds-slider-track" />
+                      <VolumeSlider.TrackFill className="vds-slider-track-fill vds-slider-track" />
+                      <VolumeSlider.Thumb className="vds-slider-thumb" />
+                    </VolumeSlider.Root>
+                  </div>
+                </div>
+                {/* ========== Central times container ========== */}
+                <div className="vds-time-group">
+                  <Time className="vds-time" type="current" />
+                  <div className="vds-time-divider">/</div>
+                  <Time className="vds-time" type="duration" />
+                </div>
+                {/* ========== Right buttons containers ========== */}
+                <div className="flex flex-row items-center">
+                  <FullscreenButton className="vds-button">
+                    {isFull ? (
+                      <FullscreenExitIcon className="fs-exit-icon vds-icon" />
+                    ) : (
+                      <FullscreenIcon className="fs-enter-icon vds-icon" />
+                    )}
+                  </FullscreenButton>
+                </div>
               </div>
-              <FullscreenButton className="vds-button">
-                {isFull ? (
-                  <FullscreenExitIcon className="fs-exit-icon vds-icon" />
-                ) : (
-                  <FullscreenIcon className="fs-enter-icon vds-icon" />
-                )}
-              </FullscreenButton>
+              <div className="time-slider">
+                <TimeSlider.Root className="vds-time-slider vds-slider">
+                  <TimeSlider.Track className="vds-slider-track" />
+                  <TimeSlider.TrackFill className="vds-slider-track-fill vds-slider-track" />
+                  <TimeSlider.Progress className="vds-slider-progress vds-slider-track" />
+                  <TimeSlider.Thumb className="vds-slider-thumb" />
+                  <TimeSlider.Preview className="vds-slider-preview">
+                    <TimeSlider.Value className="vds-slider-value" />
+                  </TimeSlider.Preview>
+                </TimeSlider.Root>
+              </div>
             </Controls.Group>
           </Controls.Root>
         </MediaPlayer>
--- src/app/player-test/page.js
+++ src/app/player-test/page.js
@@ -3,7 +3,7 @@ import Player from "@/app/player-test/Player";
 export default function playerPage() {
   return (
     <div>
-      <div className="w-300 h-200 m-auto mt-20">
+      <div className="w-full lg:w-300 lg:h-200 m-auto mt-20">
         <Player />
       </div>
     </div>
